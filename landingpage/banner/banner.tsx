ce9fe4fe89a6f1c7a0ea05148a6ebb5fbabdeac6
adhithyasimha <adhithyasimhaa@gmail.com>
2024-07-31 19:29:40 +0530

    Add .env to .gitignore

--- landingpage/banner/banner.tsx
+++ landingpage/banner/banner.tsx
@@ -16,7 +16,7 @@ export function Banner() {
     height: '100%',
   };
 
-  const autoplayPlugin = React.useMemo(() => Autoplay({ delay: 2000, stopOnInteraction: true }), []);
+  const autoplayPlugin = React.useMemo(() => Autoplay({ delay: 5000, stopOnInteraction: true }), []);
 
   return (
     <div className="banner-container">
@@ -25,19 +25,23 @@ export function Banner() {
         className="relative"
         plugins={[autoplayPlugin]}
         onMouseEnter={() => autoplayPlugin.stop()}
-        onMouseLeave={() => autoplayPlugin.reset()}
+        onMouseLeave={() => autoplayPlugin.play()}
       >
         <CarouselContent>
-          {imageUrls.map((url, index) => (
+          {imageData.map((data, index) => (
             <CarouselItem key={index} className="carousel-item">
               <div className="carousel-item-content">
                 <Card className="card">
                   <CardContent className="card-content">
                     <img
-                      src={url}
+                      src={data.url}
                       alt={`Image ${index + 1}`}
                       className="card-image"
                     />
+                    <div className="overlay">
+                      <h2 className="title">{data.title}</h2>
+                      <p className="description">{data.description}</p>
+                    </div>
                   </CardContent>
                 </Card>
               </div>
@@ -49,8 +53,20 @@ export function Banner() {
   );
 }
 
-const imageUrls = [
-  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fincinemas.sg%2Fmovieshowtimes%2FOppenheimer%2Fbanner.jpg&f=1&nofb=1&ipt=86aeeba2e0fb1b725f80406d9ffd9fb964816a299c78f1f8a7816da041fa1428&ipo=images",
-  "https://heroichollywood.com/wp-content/uploads/2016/02/Batman-v-Superman-Banner.jpg",
-  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmecinemas.com%2Fassets%2Fimg%2Fposters%2FTopGunMaveRick_banner.jpg&f=1&nofb=1&ipt=b2c235b2a14e0da4e687cd32fe4025fa412c4edbec845a5f734d9252467a3919&ipo=images"
+const imageData = [
+  {
+    url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fincinemas.sg%2Fmovieshowtimes%2FOppenheimer%2Fbanner.jpg&f=1&nofb=1&ipt=86aeeba2e0fb1b725f80406d9ffd9fb964816a299c78f1f8a7816da041fa1428&ipo=images",
+    title: "Oppenheimer",
+    description: "A film about the life of J. Robert Oppenheimer and the creation of the atomic bomb."
+  },
+  {
+    url: "https://heroichollywood.com/wp-content/uploads/2016/02/Batman-v-Superman-Banner.jpg",
+    title: "Batman v Superman",
+    description: "The epic showdown between the Dark Knight and the Man of Steel."
+  },
+  {
+    url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmecinemas.com%2Fassets%2Fimg%2Fposters%2FTopGunMaveRick_banner.jpg&f=1&nofb=1&ipt=b2c235b2a14e0da4e687cd32fe4025fa412c4edbec845a5f734d9252467a3919&ipo=images",
+    title: "Top Gun: Maverick",
+    description: "The return of Maverick in the high-flying sequel to Top Gun."
+  }
 ];
