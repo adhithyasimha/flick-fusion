119300bbddf2f09674bd8981a3926e1a8da52d3f
adhithyasimha <adhithyasimhaa@gmail.com>
2024-08-10 18:07:30 +0530

    fixed some bugs #4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // import { Landingpage } from "../../landingpage/landingpage";
 import React from "react";
 
-import {WobbleCardDemo} from "../../pages/wobble-cardd"
+
 import { Landingpage } from "../../pages/landingpage";
 export default function Home() {
   return(
