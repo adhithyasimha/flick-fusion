e10064be6d00720ccef49bf67542bb2032031f73
adhithyasimha <adhithyasimhaa@gmail.com>
2024-10-31 18:13:06 +0530

    new url incoming

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -59,7 +59,7 @@ export default function Player() {
 
   const fetchContentData = async (id: string, mediaType: string) => {
     try {
-      setPlayerUrl(`https://embed.su/embed/${mediaType}/${id}?autoplay=1`);
+      setPlayerUrl(`https://vidsrc.dev/embed/${mediaType}/${id}?autoplay=1`);
 
       // Fetch data from TMDB API
       const tmdbUrl = `https://api.themoviedb.org/3/${mediaType}/${id}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&append_to_response=credits,recommendations,seasons`;
@@ -154,7 +154,7 @@ export default function Player() {
       // Update player URL with the first episode of the selected season
       if (episodes.length > 0) {
         const firstEpisode = episodes[0].episode_number;
-        setPlayerUrl(`https://embed.su/embed/tv/${tvId}/${value}/${firstEpisode}?autoplay=1`);
+        setPlayerUrl(`https://vidsrc.dev/embed/tv/${tvId}/${value}/${firstEpisode}?autoplay=1`);
         setSelectedEpisode(firstEpisode.toString());
       }
     }
@@ -164,7 +164,7 @@ export default function Player() {
     setSelectedEpisode(value);
     const tvId = sessionStorage.getItem('contentId');
     if (tvId && selectedSeason) {
-      setPlayerUrl(`https://embed.su/embed/tv/${tvId}/${selectedSeason}/${value}?autoplay=1`);
+      setPlayerUrl(`https://vidsrc.dev/embed/tv/${tvId}/${selectedSeason}/${value}?autoplay=1`);
     }
   };
 
